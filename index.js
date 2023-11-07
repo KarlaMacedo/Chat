5f0215a486f33dbd9c9a9ed56641123da999cd1a
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-11-07 13:42:06 -0600

    Finish project

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const express = require('express');
 const path = require('path');
 const SocketIO = require('socket.io');
@@ -21,4 +22,16 @@ const io = SocketIO(server);
 //Websockets
 io.on('connection', (socket) => {
     console.log('new connection', socket.id);
+
+    // ENVIO DE MENSSAGES
+    socket.on('chat:message', (data) =>{ //servidor escucha la data que emite el usuario
+        console.log(data, 'chat message dataaaaa en index.js');
+        io.sockets.emit('chat:message', data); // despues de que el usuario emite y el servidor recibe, el servidor emite de nuevo la información a todos los sockets (usuarios conectados)
+    });
+
+    // ESCRIBIR MESSAGES
+    socket.on('chat:typing', (data) => {
+        console.log(data, 'this user is typing');
+        socket.broadcast.emit('chat:typing', data); // emitir a todos excepto a mi
+    });
 })
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,34 @@
 const socket = io();
+
+//DOM ELEMENTS
+let message = document.getElementById('message');
+let username = document.getElementById('userName');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+// ENVIAR MESSAGES
+btn.addEventListener('click', function () {
+    socket.emit('chat:message', { // usuario envia o emite los datos al servidor 
+        username: username.value,
+        message: message.value
+    })
+});
+
+socket.on('chat:message', function (data) { // usuario escucha los datos que esté enviando el servidor, despues de que el usuario envío, el servidor recibió y los reenvió; para poderla renderizar en la app
+    actions.innerHTML = '';
+    output.innerHTML += `<p>
+    <strong class="userNames">${data.username}: </strong> ${data.message}
+    </p>`
+});
+
+// ESCRIBIR MESSAGES
+message.addEventListener('keypress', function (){
+    socket.emit('chat:typing', username.value); // usuario envia
+});
+
+socket.on('chat:typing', function (data) { // usuario escucha los datos que esté enviando el servidor, despues de que el usuario envío, el servidor recibió y los reenvió; para poderla renderizar en la app
+    actions.innerHTML = `<p>
+    <em>${data} is typing a message...</em>
+    </p>`
+});
